73343ee5c25d38584246f37b39b0dad7b356da34
Hajira Mohamed Ali <hajmoham@lab3r2s8.42abudhabi.ae>
2025-04-04 10:03:15 +0400

    rank_2 pls be done

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: hajmoham <hajmoham@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/21 13:50:03 by hajmoham          #+#    #+#             */
-/*   Updated: 2025/04/03 20:01:58 by hajmoham         ###   ########.fr       */
+/*   Updated: 2025/04/04 10:02:53 by hajmoham         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ int     main(int ac, char **av)
     t_list  *stack_a;
     t_list  *stack_b;
     t_list  *clone;
-    t_list  *head_a;
+    t_list  *head_a;              // Save head for reset. just a temp variable
 
     if (ac < 2)
         return (0);
@@ -28,12 +28,12 @@ int     main(int ac, char **av)
     free_array(numbers);
     head_a = stack_a;               // Save head for reset
     clone = clone_stack(stack_a);   // Clone the stack
-    sort_clone(clone);          // Sort clone and assign pos
+    sort_clone(clone);              // Sort clone and assign pos
     if_sorted(stack_a, clone);      // Exit if already sorted
     assign_pos(stack_a, clone);     // Transfer pos to original
     free_lst(clone);                // Free the clone
     stack_a = head_a;               // Reset stack_a to head
     sort_stack(&stack_a, &stack_b);
-    free_lst(head_a);               // Free the original stack after sorting   
+    free_lst(stack_a);               // Free the stack after sorting
     return (0);
 }
